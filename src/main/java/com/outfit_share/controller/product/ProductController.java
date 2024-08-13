fbc73feb4e89474e4a3f7522f2dffda3ad05a9d3
LAPTOP-GO9QR2HU\User <s7919085@gmail.com>
2024-08-13 20:18:54 +0800

    照片的servic改寫邏輯，更新product的controller

--- src/main/java/com/outfit_share/controller/product/ProductController.java
+++ src/main/java/com/outfit_share/controller/product/ProductController.java
@@ -1,5 +1,61 @@
 package com.outfit_share.controller.product;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.outfit_share.entity.product.Product;
+import com.outfit_share.service.product.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "*") // 允許跨域請求,實際使用時應該限制為您的Vue.js應用程序的URL
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    // 創建新商品
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product savedProduct = productService.saveProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    // 更新商品
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer id, @RequestBody Product product) {
+        Product updatedProduct = productService.updateProduct(id, product);
+        if (updatedProduct != null) {
+            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // 刪除商品
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Integer id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // 獲取單個商品
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Integer id) {
+        Product product = productService.findProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // 獲取所有商品
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.findAllProduct();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
