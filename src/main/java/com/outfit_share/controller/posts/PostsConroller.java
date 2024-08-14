9cdab306e80cf045e74af705e08e934c43ae1671
ASDFGZ1\Administrator <zxzx6817619@yahoo.com.tw>
2024-08-15 00:46:35 +0800

    UserDetailService 找UserDetail+依賴Repository 加入setUserDetail ，post貼文新增成功

--- src/main/java/com/outfit_share/controller/posts/PostsConroller.java
+++ src/main/java/com/outfit_share/controller/posts/PostsConroller.java
@@ -13,107 +13,109 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.outfit_share.entity.post.Post;
+import com.outfit_share.entity.users.UserDetail;
 import com.outfit_share.service.post.PostService;
-import com.outfit_share.service.users.UsersService;
+import com.outfit_share.service.users.UserDetailService;
 
 @Controller
 public class PostsConroller {
 
-	// @Autowired
-	// private PostsService postsService;
+	 @Autowired
+	 private PostService postService;
 
-	// @Autowired
-	// private UsersService usersService;
+	 @Autowired
+	 private UserDetailService userDService;
 
-	// @GetMapping("/posts/add")
-	// public String addPosts(Model model) {
-	// // Integer currentUserId = getCurrentUserId();//實現此方法以獲取當前用戶 ID
-	// // model.addAttribute("currentUserId", currentUserId);
-	// return "posts/addPosts";
-	// }
+	 @GetMapping("/posts/add")
+	 public String addPosts(Model model) {
+//	  Integer currentUserId = getCurrentUserId();//實現此方法以獲取當前用戶 ID
+//	  model.addAttribute("currentUserId", currentUserId);
+	 return "posts/addPosts";
+	 }
 
-	// @PostMapping("/posts/addPosts")
-	// public String posts(
-	// @RequestParam Integer userId,
-	// @RequestParam String contenttype,
-	// @RequestParam String posttitle,
-	// @RequestParam String contenttext,
-	// @RequestParam(required = false) @DateTimeFormat(iso =
-	// DateTimeFormat.ISO.DATE_TIME) Date createdat,
-	// @RequestParam(required = false) @DateTimeFormat(iso =
-	// DateTimeFormat.ISO.DATE_TIME) Date deletedat,
-	// Model model) {
+	 @PostMapping("/posts/addPosts")
+	 public String posts(
+	 @RequestParam Integer userId,
+	 @RequestParam String contenttype,
+	 @RequestParam String posttitle,
+	 @RequestParam String contenttext,
+	 @RequestParam(required = false) @DateTimeFormat(iso =
+	 DateTimeFormat.ISO.DATE_TIME) Date createdat,
+	 @RequestParam(required = false) @DateTimeFormat(iso =
+	 DateTimeFormat.ISO.DATE_TIME) Date deletedat,
+	 Model model) {
 
-	// // Integer userIdInt = Integer.valueOf(userId); // 轉換為 Integer
-	// // System.out.println("userId: " + userId);
-	// Users user = usersService.findUsersById(userId);
-	// if (userId == null) {
-	// model.addAttribute("錯誤", "未找到用戶");
-	// return "posts/addPosts";
-	// }
+	 // Integer userIdInt = Integer.valueOf(userId); // 轉換為 Integer
+	 // System.out.println("userId: " + userId);
+	 UserDetail user = userDService.findUserById(userId);
+	 if (user  == null) {
+	 model.addAttribute("錯誤", "未找到用戶");
+	 return "posts/addPosts";
+	 }
 
-	// Posts posts = new Posts();
-	// posts.setUsers(user);
-	// posts.setContentType(contenttype);
-	// posts.setContentText(contenttext);
-	// posts.setPostTitle(posttitle);
-	// posts.setCreatedAt(createdat);
-	// posts.setDeletedAt(null);// 尚未刪除
+	 Post post = new Post();
+	 post.setPostId(userId);
+	 post.setUserDetail(user);
+	 post.setContentType(contenttype);
+	 post.setContentText(contenttext);
+	 post.setPostTitle(posttitle);
+	 post.setCreatedAt(createdat);
+	 post.setDeletedAt(null);// 尚未刪除
 
-	// postsService.createPosts(posts);
+	 postService.createPost(post);
+	 
+	 model.addAttribute("OK", "新增成功");
+	 return "posts/addPosts";
+	 }
 
-	// model.addAttribute("OK", "新增成功");
-	// return "posts/addPosts";
-	// }
+	 @GetMapping("/posts/list")
+	 public String findAll(Model model) {
+	 List<Post> list = postService.findAllPost();
 
-	// @GetMapping("/posts/list")
-	// public String findAll(Model model) {
-	// List<Posts> list = postsService.findAllPosts();
+	 model.addAttribute("posts", list);
+	 return "posts/showPosts";
+	 }
 
-	// model.addAttribute("posts", list);
-	// return "posts/showPosts";
-	// }
+	 @GetMapping("/posts/update")
+	 public String updatePosts(@RequestParam Integer id, Model model) {
+	 Post post = postService.findPostById(id);
+	 if (post == null) {
+	 model.addAttribute("錯誤", "未找到貼文");
+	 return "posts/list";
+	 }
+	 model.addAttribute("posts", post);
+	 return "posts/upPosts";
+	 }
 
-	// @GetMapping("/posts/update")
-	// public String updatePosts(@RequestParam Integer id, Model model) {
-	// Posts posts = postsService.findPostsById(id);
-	// if (posts == null) {
-	// model.addAttribute("錯誤", "未找到貼文");
-	// return "posts/list";
-	// }
-	// model.addAttribute("posts", posts);
-	// return "posts/upPosts";
-	// }
+	 @PostMapping("/posts/updatePoasts")
+	 public String updatePoastspage(@ModelAttribute Post post, Model model) {
+	 Post existingPost = postService.findPostById(post.getPostId());
 
-	// @PostMapping("/posts/updatePoasts")
-	// public String updatePoastspage(@ModelAttribute Posts posts, Model model) {
-	// Posts existingPost = postsService.findPostsById(posts.getPostId());
+	 if (existingPost != null) {
+	 model.addAttribute("錯誤", "未找到貼文");
+	 return "posts/list";
+	 }
+	 // 更新貼文資料
+	 existingPost.setContentText(post.getContentText());
+	 existingPost.setContentType(post.getContentType());
+	 existingPost.setPostTitle(post.getPostTitle());
+	 existingPost.setDeletedAt(post.getDeletedAt()); // 可選：設定刪除時間
 
-	// if (existingPost != null) {
-	// model.addAttribute("錯誤", "未找到貼文");
-	// return "posts/list";
-	// }
-	// // 更新貼文資料
-	// existingPost.setContentText(posts.getContentText());
-	// existingPost.setContentType(posts.getContentType());
-	// existingPost.setPostTitle(posts.getPostTitle());
-	// existingPost.setDeletedAt(posts.getDeletedAt()); // 可選：設定刪除時間
+	 postService.createPost(existingPost);
 
-	// postsService.createPosts(existingPost);
+	 return "redirect:/posts/list";
+	 }
 
-	// return "redirect:/posts/list";
-	// }
+	 @PostMapping("/posts/delete")
+	 public String deletePost(@RequestParam Integer id, Model model) {
+	 Post post = postService.findPostById(id);
+	 if (post == null) {
+	 model.addAttribute("錯誤", "未找到貼文");
+	 return "posts/list";
+	 }
 
-	// @PostMapping("/posts/delete")
-	// public String deletePost(@RequestParam Integer id, Model model) {
-	// Posts post = postsService.findPostsById(id);
-	// if (post == null) {
-	// model.addAttribute("錯誤", "未找到貼文");
-	// return "posts/list";
-	// }
+	 postService.deletePostById(id);
 
-	// postsService.deletePostsById(id);
-
-	// return "redirect:/posts/list";
-	// }
+	 return "redirect:/posts/list";
+	 }
 }
--- src/main/java/com/outfit_share/service/users/UserDetailService.java
+++ src/main/java/com/outfit_share/service/users/UserDetailService.java
@@ -1,8 +1,19 @@
 package com.outfit_share.service.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.outfit_share.entity.users.UserDetail;
+import com.outfit_share.repository.users.UserDetailRepository;
 
 @Service
 public class UserDetailService {
+
+	@Autowired
+    private UserDetailRepository userDetailRepository;
+	
+	public UserDetail findUserById(Integer userId) {
+		return userDetailRepository.findById(userId).orElse(null);
+	}
 
 }
