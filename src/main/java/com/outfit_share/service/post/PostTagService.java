862abecdbb2224e8936d9398cd47f4332f50de51
MaLingJi <a385500062@gmail.com>
2024-08-19 04:29:48 +0800

    新增PostTagsDTO與相關變更
    (cherry picked from commit 2611ca6833e1d6574aa93ef045a5fbc4fe1644d0)

--- src/main/java/com/outfit_share/service/post/PostTagService.java
+++ src/main/java/com/outfit_share/service/post/PostTagService.java
@@ -1,5 +1,62 @@
 package com.outfit_share.service.post;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.outfit_share.entity.post.PostTags;
+import com.outfit_share.entity.post.PostTagsDTO;
+import com.outfit_share.repository.post.PostTagsRepository;
+
+@Service
+@Transactional
 public class PostTagService {
 
+    @Autowired
+    private PostTagsRepository postTagsRepository;
+
+    public PostTags savePostTags(PostTags postTags) {
+        return postTagsRepository.save(postTags);
+    }
+
+    public List<PostTagsDTO> findByPostPostId(Integer postId) {
+        List<PostTags> result = postTagsRepository.findByPostPostId(postId);
+        List<PostTagsDTO> dtoList = new ArrayList<>();
+
+        for (PostTags postTag : result) {
+            Hibernate.initialize(postTag.getPost());
+            PostTagsDTO postTagsDTO = new PostTagsDTO(postTag);
+            dtoList.add(postTagsDTO);
+        }
+        return dtoList;
+    }
+
+    public List<PostTagsDTO> findByTagsId(Integer Id) {
+        List<PostTags> result = postTagsRepository.findByTagsId(Id);
+        List<PostTagsDTO> dtoList = new ArrayList<>();
+
+        for (PostTags postTag : result) {
+            Hibernate.initialize(postTag.getTags());
+            PostTagsDTO postTagsDTO = new PostTagsDTO(postTag);
+            dtoList.add(postTagsDTO);
+        }
+        return dtoList;
+    }
+
+    public List<PostTagsDTO> findAll() {
+        List<PostTags> result = postTagsRepository.findAll();
+        List<PostTagsDTO> dtoList = new ArrayList<>();
+
+        for (PostTags postTag : result) {
+            Hibernate.initialize(postTag.getTags());
+            Hibernate.initialize(postTag.getPost());
+            PostTagsDTO postTagsDTO = new PostTagsDTO(postTag);
+            dtoList.add(postTagsDTO);
+        }
+        return dtoList;
+    }
 }
