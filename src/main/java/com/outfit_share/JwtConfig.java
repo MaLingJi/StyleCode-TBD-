38804808ec8f5c6479a5f6414e46de27fd955d04
LAPTOP-GO9QR2HU\User <s7919085@gmail.com>
2024-08-22 21:55:44 +0800

    +了addCorsMappings

--- src/main/java/com/outfit_share/JwtConfig.java
+++ src/main/java/com/outfit_share/JwtConfig.java
@@ -2,6 +2,7 @@ package com.outfit_share;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,5 +26,14 @@ public class JwtConfig implements WebMvcConfigurer {
         		.addPathPatterns("/order/admin/**")
         		.addPathPatterns("/order/**");
 	
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 允许的前端源
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
