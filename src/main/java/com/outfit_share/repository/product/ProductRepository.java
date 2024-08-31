698d089fef271927ae55f11008dbecc3be6ba73e
yu <s7919085@gmail.com>
2024-08-31 18:19:38 +0800

    新增  排序 和 模糊搜尋功能

--- src/main/java/com/outfit_share/repository/product/ProductRepository.java
+++ src/main/java/com/outfit_share/repository/product/ProductRepository.java
@@ -22,13 +22,29 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	@Query("from Product where productName like %:name%")
 	List<Product> findByNameLikeQuery(@Param("name") String name);
 
-	// 模糊搜尋 && 價格由高到低||由低到高 && 全部商品
-//	@Query("SELECT p FROM Product p JOIN p.productDetails pd WHERE " + "(:name IS NULL OR p.productName LIKE %:name%) "
-//			+ "ORDER BY CASE WHEN :sort = 'priceAsc' THEN pd.price END ASC, "
-//			+ "CASE WHEN :sort = 'priceDesc' THEN pd.price END DESC")
-//	List<Product> findByNameAndSort(@Param("name") String name, @Param("sort") String sort);
-//	
-//	
+	// 價格排序
+	@Query("SELECT p FROM Product p ORDER BY p.price ASC")
+	List<Product> findAllSortedByPriceAsc();
+
+	@Query("SELECT p FROM Product p ORDER BY p.price DESC")
+	List<Product> findAllSortedByPriceDesc();
+
+	
+	// 按分類和價格排序
+	@Query("SELECT p FROM Product p WHERE p.subcategoryId.category.categoryId = :categoryId ORDER BY p.price ASC")
+	List<Product> findByCategoryIdOrderByPriceAsc(@Param("categoryId") Integer categoryId);
+
+	@Query("SELECT p FROM Product p WHERE p.subcategoryId.category.categoryId = :categoryId ORDER BY p.price DESC")
+	List<Product> findByCategoryIdOrderByPriceDesc(@Param("categoryId") Integer categoryId);
+
+	// 按子分類和價格排序
+	@Query("SELECT p FROM Product p WHERE p.subcategoryId.subcategoryId = :subcategoryId ORDER BY p.price ASC")
+	List<Product> findBySubcategoryIdOrderByPriceAsc(@Param("subcategoryId") Integer subcategoryId);
+
+	@Query("SELECT p FROM Product p WHERE p.subcategoryId.subcategoryId = :subcategoryId ORDER BY p.price DESC")
+	List<Product> findBySubcategoryIdOrderByPriceDesc(@Param("subcategoryId") Integer subcategoryId);
+	
+	
 	
 	// 模糊搜尋 && 價格由高到低||由低到高 && 全部商品
 	@Query("SELECT p FROM Product p WHERE (:name IS NULL OR p.productName LIKE %:name%) ORDER BY "
